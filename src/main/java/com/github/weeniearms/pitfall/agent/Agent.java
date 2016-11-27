8a771c33d536aa745cac985867ec68b42c4a3204
Adrian Pluta <phoenix@abweb.pl>
2016-11-27 18:15:50 +0100

    Added configuration and registration

--- src/main/java/com/github/weeniearms/pitfall/agent/Agent.java
+++ src/main/java/com/github/weeniearms/pitfall/agent/Agent.java
@@ -1,11 +1,15 @@
 package com.github.weeniearms.pitfall.agent;
 
+import com.github.weeniearms.pitfall.agent.configuration.agent.AgentConfiguration;
+import com.github.weeniearms.pitfall.agent.configuration.agent.AgentConfigurationParser;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.utility.JavaModule;
+import okhttp3.*;
 
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 import java.util.Arrays;
 
@@ -20,7 +24,30 @@ public class Agent {
                             .transform((builder, type, classLoader) -> builder.method(i.getMethodMatcher())
                                     .intercept(MethodDelegation.to(i))
                             ).installOn(instrumentation);
+
+                    registerInterceptor(i);
                 });
+    }
+
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    private static void registerInterceptor(Interceptor interceptor) {
+        AgentConfiguration agentConfiguration = new AgentConfigurationParser().parse();
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON, "{\"test\": 123}");
+        Request request = new Request.Builder()
+                .url(agentConfiguration.getServer() + String.format("/groups/%s/apps/%s/interceptors", agentConfiguration.getGroup(), agentConfiguration.getName()))
+                .post(body)
+                .build();
+        
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static class LoggingListener implements AgentBuilder.Listener {
---
+++ src/main/java/com/github/weeniearms/pitfall/agent/configuration/agent/AgentConfiguration.java
@@ -0,0 +1,12 @@
+package com.github.weeniearms.pitfall.agent.configuration.agent;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AgentConfiguration {
+    private String server;
+    private String group;
+    private String name;
+}
